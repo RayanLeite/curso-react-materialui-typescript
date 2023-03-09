72789cee5ffb4bcb98e40705d5ea01a203f44149
Rayan Leite <rayan.teixeira@poli.digital>
2023-03-09 17:03:47 -0300

    feat: adicionado react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./routes";
+
 export const App = () => {
-  return <div className="App">Teste</div>;
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <AppRoutes />
+      </div>
+    </BrowserRouter>
+  );
 };
