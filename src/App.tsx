90fb776682785dd8bfaebb14a7f4b8b663e5af88
Rayan Leite <rayan.teixeira@poli.digital>
2023-03-11 11:52:09 -0300

    feat: Adicionado tema claro e provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import { ThemeProvider } from "@mui/material";
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
+import { LigthTheme } from "./shared/themes";
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <AppRoutes />
-      </div>
-    </BrowserRouter>
+    <ThemeProvider theme={LigthTheme}>
+      <BrowserRouter>
+        <div className="App">
+          <AppRoutes />
+        </div>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 };
