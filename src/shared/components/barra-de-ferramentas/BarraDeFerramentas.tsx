b7476ccd01d1fa723c39f7a326f897a381272562
RayanTeixeira <rayan.teixeira@poli.digital>
2023-04-10 17:44:53 -0300

    feat: barra de ferramentas da listagem

---
+++ src/shared/components/barra-de-ferramentas/BarraDeFerramentas.tsx
@@ -0,0 +1,59 @@
+import { Add } from "@mui/icons-material";
+import { Box, Button, Paper, TextField, useTheme } from "@mui/material";
+import React from "react";
+
+interface IBarraDeFerramentas {
+  textoDaBusca?: string;
+  mostrarInputBusca?: boolean;
+  aoMudarTextoDeBusca?: (novoTexto: string) => void;
+  textoBotaoNovo?: string;
+  mostrarBotaoNovo?: boolean;
+  aoClicarEmNovo?: () => void;
+}
+
+export const BarraDeFerramentas: React.FC<IBarraDeFerramentas> = ({
+  textoDaBusca = "",
+  mostrarInputBusca = false,
+  aoMudarTextoDeBusca,
+  aoClicarEmNovo,
+  textoBotaoNovo = "Novo",
+  mostrarBotaoNovo = true,
+}) => {
+  const theme = useTheme();
+
+  return (
+    <Box
+      component={Paper}
+      height={theme.spacing(5)}
+      marginX={1}
+      padding={1}
+      paddingX={2}
+      display="flex"
+      gap={1}
+      alignItems="center"
+    >
+      {mostrarInputBusca && (
+        <TextField
+          size="small"
+          placeholder="Pesquisar..."
+          value={textoDaBusca}
+          onChange={(e) => aoMudarTextoDeBusca?.(e.target.value)}
+        />
+      )}
+
+      <Box flex={1} display="flex" justifyContent="end">
+        {mostrarBotaoNovo && (
+          <Button
+            variant="contained"
+            disableElevation
+            color="primary"
+            endIcon={<Add />}
+            onClick={aoClicarEmNovo}
+          >
+            {textoBotaoNovo}
+          </Button>
+        )}
+      </Box>
+    </Box>
+  );
+};
